91bc053434bd97f72626fc3ed5d75b7209429220
권민수 <kd0615@naver.com>
2023-05-15 12:02:04 +0900

    테이블뷰컨트롤러 룰렛 기능 추가

--- TableViewController/AddViewController.swift
+++ TableViewController/AddViewController.swift
@@ -7,13 +7,28 @@
 
 import UIKit
 
-class AddViewController: UIViewController {
+class AddViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    let MAX_ARRAY_NUM = 3
+    let PICKER_VIEW_COLUMN = 1
+    let PICKER_VIEW_HEIGHT:CGFloat = 50
+    let PICKER_VIEW_WIDTH:CGFloat = 50
+    var imageArray = [UIImage?]()
+    var imageFileName = ["cart.png", "clock.png", "pencil.png"]
 
     @IBOutlet var tfAddItem: UITextField!
-    
+    @IBOutlet var pickerImage: UIPickerView!
+    @IBOutlet var selectedImageView: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        for i in 0 ..< MAX_ARRAY_NUM {
+            let image = UIImage(named: imageFileName[i])
+            imageArray.append(image)
+        }
+        
+        selectedImageView.image = imageArray[0]
 
         // Do any additional setup after loading the view.
     }
@@ -23,6 +38,33 @@ class AddViewController: UIViewController {
         itemsImageFile.append("clock.png")
         tfAddItem.text=""
         _ = navigationController?.popViewController(animated: true)
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return PICKER_VIEW_COLUMN
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, rowHeightForComponent component: Int) -> CGFloat {
+        return PICKER_VIEW_HEIGHT
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, widthForComponent component: Int) -> CGFloat {
+        return PICKER_VIEW_WIDTH
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return imageFileName.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, viewForRow row: Int, forComponent component: Int, reusing view: UIView?) -> UIView {
+        let imageView = UIImageView(image: imageArray[row])
+        imageView.frame = CGRect(x: 0, y: 0, width: 50, height: 50)
+        
+        return imageView
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        selectedImageView.image = imageArray[row]
     }
     
     /*
